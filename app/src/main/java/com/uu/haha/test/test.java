7afb411261e33ca3c1d5a39a88f019b6a31cf524
lusy2 <cloud0000000@126.com>
2015-12-03 20:19:45 +0800

    test button

--- app/src/main/java/com/uu/haha/test/test.java
+++ app/src/main/java/com/uu/haha/test/test.java
@@ -1,9 +1,14 @@
 package com.uu.haha.test;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class test extends AppCompatActivity {
 
@@ -11,6 +16,15 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Button bt =(Button)findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 
 
